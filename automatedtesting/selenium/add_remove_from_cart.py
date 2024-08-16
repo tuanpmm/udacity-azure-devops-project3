6c0630c98344816f342a2f328ad865dd2aff407c
tuanpm14 <tuanpm14@outlook.com>
2024-08-17 04:18:09 +0700

    Update add_remove_from_cart.py

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
+import datetime
 
 # --uncomment when running in Azure DevOps.
 options = ChromeOptions()    
@@ -15,8 +16,8 @@ totalItems = 6
 
 # Start the browser and login with standard_user
 def login (user, password):
-    print ('Starting the browser...')
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    print (log_timestamp() + 'Starting the browser...')
+    print (log_timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
     driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
@@ -26,7 +27,7 @@ def login (user, password):
     el_products_label = driver.find_element(By.CSS_SELECTOR, "span.title").text
     assert 'Products' in el_products_label
 
-    print ('User is login successfully: username {:s}, password {:s}.'.format(user, password))
+    print (log_timestamp() + 'User is login successfully: username {:s}, password {:s}.'.format(user, password))
 
 def add_cart():
     for i in range(totalItems):
@@ -39,7 +40,7 @@ def add_cart():
         el_item_name = "div.inventory_details_name"
         item_name = driver.find_element(By.CSS_SELECTOR, el_item_name).text
 
-        print('{:s} item was added to shopping cart.'.format(item_name))
+        print(log_timestamp() + '{:s} item was added to shopping cart.'.format(item_name))
 
         el_back_to_products = "button[id='back-to-products']"
         driver.find_element(By.CSS_SELECTOR, el_back_to_products).click()
@@ -47,7 +48,7 @@ def add_cart():
     el_shopping_cart_container = "div[id='shopping_cart_container'] > a > span"
     total_items = driver.find_element(By.CSS_SELECTOR, el_shopping_cart_container).text
     assert totalItems == int(total_items)
-    print ('Total item added to shopping cart is: {:s}.'.format(total_items))
+    print (log_timestamp() + 'Total item added to shopping cart is: {:s}.'.format(total_items))
 
 def remove_cart():
     for i in range(totalItems):
@@ -60,7 +61,7 @@ def remove_cart():
         el_remove = "button[id='remove']"
         driver.find_element(By.CSS_SELECTOR, el_remove).click()
 
-        print('{:s} item was removed from shopping cart.'.format(item_name))
+        print(log_timestamp() + '{:s} item was removed from shopping cart.'.format(item_name))
 
         el_back_to_products = "button[id='back-to-products']"
         driver.find_element(By.CSS_SELECTOR, el_back_to_products).click()
@@ -68,10 +69,14 @@ def remove_cart():
     el_shopping_cart_container = "div[id='shopping_cart_container'] > a"
     total_items = driver.find_element(By.CSS_SELECTOR, el_shopping_cart_container).text
     assert '' == total_items
-    print ('All items were removed from shopping cart.')
+    print (log_timestamp() + 'All items were removed from shopping cart.')
 
-print ('Start Selenium Tests...')
+def log_timestamp():
+    ts = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    return (ts + ' ')
+
+print (log_timestamp() + 'Start Selenium Tests...')
 login('standard_user', 'secret_sauce')
 add_cart()
 remove_cart()
-print ('Completed Selenium Tests.')
+print (log_timestamp() + 'Completed Selenium Tests.')
