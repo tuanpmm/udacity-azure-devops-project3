63825aeabb7538a3b325cbac57f0f89793eaec59
tuanpm14 <tuanpm14@outlook.com>
2024-08-16 23:08:01 +0700

    Update add_remove_from_cart.py

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.by import By
 
 # --uncomment when running in Azure DevOps.
 options = ChromeOptions()
-options.add_argument("--headless") 
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_argument("--remote-debugging-port=9222")
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 totalItems = 6
 
